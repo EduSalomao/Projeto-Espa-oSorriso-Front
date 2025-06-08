04044861aec233b3325f1c475e1642980add758f
Luiz <luizcaetite@gmail.com>
2025-06-08 20:24:08 -0300

    Adicionando rotas para gerenciamento de procedimentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PatientForm from './components/Patient/PatientForm'
-import PatientList from './components/Patient/PatientList/PatientList'
-import Dashboard from './components/dashboard/Dashboard'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Pacientes from "./pages/pacientes";
 import Dentistas from "./pages/dentistas";
+import Procedimentos from "./pages/procedimentos";
 import MainLayout from "./layout/MainLayout";
-import PacienteDetails from './pages/pacientes/PacienteDetails/PacienteDetails'
-import DentistaDetails from './pages/dentistas/DentistaDetails'
+import PacienteDetails from './pages/pacientes/PacienteDetails/PacienteDetails';
+import DentistaDetails from './pages/dentistas/DentistaDetails';
+import ProcedimentoDetails from "./pages/procedimentos/ProcedimentosDetails";
+
 function App() {
   return (
     <BrowserRouter>
@@ -21,13 +18,12 @@ function App() {
           <Route path="pacientes/:id" element={<PacienteDetails />} />
           <Route path="dentistas" element={<Dentistas />} />
           <Route path="dentistas/:id" element={<DentistaDetails />} />
-
+          <Route path="procedimentos" element={<Procedimentos />} />
+          <Route path="procedimentos/:id" element={<ProcedimentoDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-
-          //<Route path="dentistas" element={<Dentistas />} />
 export default App;
