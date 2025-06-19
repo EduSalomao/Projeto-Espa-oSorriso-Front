b076057ea5bce13e73571ad77caea7c924d89d4f
Franck Allyson <franckallyson@hotmail.com>
2025-06-19 15:54:07 -0300

    [Franck] Corrigind overflow container

--- src/components/Containers/ContainerContent.style.ts
+++ src/components/Containers/ContainerContent.style.ts
@@ -11,20 +11,18 @@ export const MainContainerContent = styled.div`
 `;
 
 export const MainContainerAgendaContent = styled.div`
-
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  width: 100%;
-  align-items: center;
-`;
-export const ContainerAgenda = styled.div`  
-  
+  max-height: 88vh;
   overflow-y: auto;
   overflow-x: hidden;
   scrollbar-width: thin;
   scrollbar-color: #3a5289 #EDF2FD;
 
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+export const ContainerAgenda = styled.div`  
+  
   width: 80%;
   padding-top: 40px;
 `
